20c4446ead3487f7d44974812bca619fea450fac
Yu Assai <julyshb@yandex.ru>
2026-03-25 02:55:06 +0300

    Обновлён ecosystem.config.js для деплоя

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -1,5 +1,6 @@
 const dotenv = require('dotenv');
-dotenv.config({ path: '../.env.deploy' });
+const path = require('path');
+dotenv.config({ path: path.join(__dirname, '../.env.deploy') });
 
 module.exports = {
   apps: [{
@@ -17,8 +18,9 @@ module.exports = {
       ref: `origin/${process.env.BRANCH}`,
       repo: process.env.REPO_PATH,
       path: process.env.BACKEND_PATH,
-      'pre-deploy': `scp -r .env ${process.env.USER}@${process.env.SERVER_IP}:${process.env.BACKEND_PATH}/current/backend/`,
+      'pre-deploy': `scp ${path.join(__dirname, '.env')} ${process.env.USER}@${process.env.SERVER_IP}:${process.env.BACKEND_PATH}/current/backend/`,
       'post-deploy': 'cd backend && npm ci && npm run build && pm2 startOrRestart ecosystem.config.js --env production',
+      'ssh_options': 'StrictHostKeyChecking=no'
     },
   },
 };
