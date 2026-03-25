f69023c74f1da456cff0667873c840bd0f14470c
Yu Assai <julyshb@yandex.ru>
2026-03-25 21:56:12 +0300

    Подготовка к деплою бека

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -21,7 +21,6 @@ module.exports = {
       path: BACKEND_PATH,
       'pre-deploy-local': `bash scripts/deployEnv.sh ${USER}@${SERVER_IP} ${BACKEND_PATH}`,
       'post-deploy': 'cd backend && npm ci && npm run build && pm2 startOrRestart ecosystem.config.js --env production',
-      'ssh_options': 'StrictHostKeyChecking=no'
     },
   },
 };
