cd846615a19b27c78b89334513aedb2eec4fba0d
Yu Assai <julyshb@yandex.ru>
2026-03-25 03:29:46 +0300

    Новая проба

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -17,7 +17,7 @@ module.exports = {
       ref: `origin/${process.env.BRANCH}`,
       repo: process.env.REPO_PATH,
       path: process.env.BACKEND_PATH,
-      'pre-deploy': `bash scripts/deploy.sh ${process.env.USER}@${process.env.SERVER_IP} ${process.env.BACKEND_PATH}`,
+      'pre-deploy': `bash scripts/deployEnv.sh ${process.env.USER}@${process.env.SERVER_IP} ${process.env.BACKEND_PATH}`,
       'post-deploy': 'cd backend && npm ci && npm run build && pm2 startOrRestart ecosystem.config.js --env production',
       'ssh_options': 'StrictHostKeyChecking=no'
     },
