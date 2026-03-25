0bccdbf9fbbf0b1acc87ee470b18f31e2188e5ca
Yu Assai <julyshb@yandex.ru>
2026-03-25 03:22:02 +0300

    Устранение проблемы с путем'

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -1,6 +1,5 @@
 const dotenv = require('dotenv');
-const path = require('path');
-dotenv.config({ path: path.join(__dirname, '../.env.deploy') });
+dotenv.config({ path: '../.env.deploy' });
 
 module.exports = {
   apps: [{
@@ -18,7 +17,7 @@ module.exports = {
       ref: `origin/${process.env.BRANCH}`,
       repo: process.env.REPO_PATH,
       path: process.env.BACKEND_PATH,
-      'pre-deploy': `scp "${path.join(__dirname, '.env')}" ${process.env.USER}@${process.env.SERVER_IP}:${process.env.BACKEND_PATH}/current/backend/`,
+      'pre-deploy': `bash scripts/deploy.sh ${process.env.USER}@${process.env.SERVER_IP} ${process.env.BACKEND_PATH}`,
       'post-deploy': 'cd backend && npm ci && npm run build && pm2 startOrRestart ecosystem.config.js --env production',
       'ssh_options': 'StrictHostKeyChecking=no'
     },
